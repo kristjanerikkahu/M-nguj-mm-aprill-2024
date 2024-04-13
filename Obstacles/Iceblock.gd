2ae7c36a8e4dca7d0f249d34321ea2c5ff19f58f
RaivoPoisu <anold.friendooste@gmail.com>
2024-04-13 13:53:59 +0300

    proov??

--- Obstacles/Iceblock.gd
+++ Obstacles/Iceblock.gd
@@ -1,11 +1,16 @@
 extends Node2D
-class_name Sun
+
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	await get_tree().create_timer(10).timeout
-	queue_free()
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_area_2d_area_entered(area):
+	$StaticBody2D/Sprite2D
