4a8064d327d9c75e358078153f717d7411a7020f
RichardMJaks <richardmiikael.jaks@gmail.com>
2024-04-14 14:12:14 +0300

    Added font@

--- Global Script/gamemaster.gd
+++ Global Script/gamemaster.gd
@@ -1,5 +1,12 @@
 extends Node
 
+var timer_in_seconds = 0
+var timing : bool = false
+
+func _process(delta):
+	if timing:
+		timer_in_seconds += delta
+
 func player_death():
 	var timer = get_tree().create_timer(2)
 	await timer.timeout
--- Global Script/spawn_point.gd
+++ Global Script/spawn_point.gd
@@ -2,10 +2,10 @@ extends Marker2D
 
 @export var player : PackedScene
 
+
 func resurrect_player():
 	$Particles.emitting = true
 	await get_tree().create_timer(0.8).timeout
 	var p = player.instantiate()
 	p.position = global_position
 	get_tree().current_scene.add_child(p)
-
